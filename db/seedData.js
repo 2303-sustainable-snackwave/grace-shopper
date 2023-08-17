f46e9534112ca99e564aad243144b950f9ae0117
TripolarHZ <hzha627@emory.edu>
2023-08-17 19:32:59 -0400

    finished seedData

--- db/seedData.js
+++ db/seedData.js
@@ -1,16 +1,16 @@
-import {faker} from '@faker-js/faker';
-import {v4} from 'uuid';
-import slugify from 'slugify';
+const {faker} = require('@faker-js/faker');
+const {v4} = require('uuid')
+const {createProducts, getAllProducts} = require('./models/products')
 
-export const bikeData = () => {
+const bikeData = () => {
     const category = faker.vehicle.bicycle();
-    const name = name + " " + faker.vehicle.model();
-    const handle = slugify(name).toLocaleLowerCase();
-    const minPrice = faker.datatype.number({
+    let name = "";
+    name = name + " " + faker.vehicle.model();
+    const minPrice = faker.number.int({
         min: 500,
         max: 20000,
     });
-    const maxPrice = faker.datatype.number({
+    const maxPrice = faker.number.int({
         min: minPrice,
         max: minPrice * 3
     });
@@ -20,7 +20,7 @@ export const bikeData = () => {
        category: category,
        brand: faker.vehicle.manufacturer(),
        name: name, 
-       image_url: faker.image.bicycle(),
+       image_url: faker.image.urlLoremFlickr({ category: 'bicycle' }),
        description: faker.lorem.paragraph(),
        min_price: {
         amount: minPrice,
@@ -31,11 +31,20 @@ export const bikeData = () => {
         currency_code: "USD",
        },
        currency_code: "USD",
-       amount: faker.datatype.number({
+       amount: faker.number.int({
         min: 500,
         max: 60000,
        }),
        availability: faker.datatype.boolean(),
-       total_inventory: faker.datatype.number({ min: 1, max: 5 }),
+       total_inventory: faker.number.int({ min: 1, max: 5 }),
     }
 }
+
+async function fillDB(numSamples) {
+    for (let i = 0; i < numSamples; i++) {
+      const productData = bikeData();
+      await createProducts(productData);
+    }
+  }
+
+fillDB(25);

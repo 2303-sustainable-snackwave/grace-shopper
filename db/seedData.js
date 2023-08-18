7c403e95cb6e850c1aa52f3178f21233ab0dbed9
TripolarHZ <hzha627@emory.edu>
2023-08-17 20:36:59 -0400

    finished product table creating and dropping

--- db/seedData.js
+++ db/seedData.js
@@ -1,8 +1,10 @@
 const {faker} = require('@faker-js/faker');
 const {v4} = require('uuid')
 const {createProducts, getAllProducts} = require('./models/products')
+const client = require('./client');
 
 const bikeData = () => {
+    console.log("Generating bike data...");
     const category = faker.vehicle.bicycle();
     let name = "";
     name = name + " " + faker.vehicle.model();
@@ -20,7 +22,7 @@ const bikeData = () => {
        category: category,
        brand: faker.vehicle.manufacturer(),
        name: name, 
-       image_url: faker.image.urlLoremFlickr({ category: 'bicycle' }),
+       imageUrl: faker.image.urlLoremFlickr({ category: 'bicycle' }),
        description: faker.lorem.paragraph(),
        min_price: {
         amount: minPrice,
@@ -40,11 +42,99 @@ const bikeData = () => {
     }
 }
 
+
+async function dropTables() {
+  try {
+    console.log("Starting to drop table...");
+
+    // Add your SQL query to drop the products table here
+    await client.query(`
+      DROP TABLE IF EXISTS products;
+    `);
+
+    console.log("Finished dropping table!");
+  } catch (error) {
+    console.error("Error dropping table!");
+    throw error;
+  }
+}
+
+
+async function createTables() {
+  try {
+    console.log("Starting to create table...");
+
+    // Add your SQL query to create the products table here
+    await client.query(`
+      CREATE TABLE products (
+        id UUID PRIMARY KEY,
+        category TEXT,
+        brand TEXT,
+        name TEXT,
+        imageUrl TEXT,
+        description TEXT,
+        min_price JSONB,
+        max_price JSONB,
+        currency_code TEXT,
+        amount INT,
+        availability BOOLEAN,
+        total_inventory INT
+    );
+  `);
+
+  console.log("Finished creating table!");
+} catch (error) {
+  console.error("Error creating table!");
+  throw error;
+}
+}
+
 async function fillDB(numSamples) {
+  try {
+    console.log("Starting to fill database...");
+
     for (let i = 0; i < numSamples; i++) {
       const productData = bikeData();
       await createProducts(productData);
     }
-  }
 
-fillDB(25);
+    console.log("Finished filling database!");
+  } catch (error) {
+    console.error("Error filling database!");
+    throw error;
+  }
+}
+
+async function rebuildDB() {
+  try {
+      client.connect();
+
+      await dropTables();
+      await createTables();
+      await fillDB(10);
+  }   catch(error) {
+      console.log("Error during rebuildDB")
+      throw error;
+  }
+}
+
+async function testDB() {
+  try {
+    console.log("Starting to test database...");
+
+    console.log("Calling getAllProducts");
+    const products = await getAllProducts();
+    console.log("Result:", products);
+
+    console.log("Finished database tests!");
+  } catch (error) {
+    console.log("Error during testDB");
+    throw error;
+  }
+}
+
+
+rebuildDB()
+    .then(testDB)
+    .catch(console.error)
+    .finally(() => client.end());

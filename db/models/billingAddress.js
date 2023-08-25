6bed9fac10728d431f70e2c5bf4c32fb84d4501a
Cameron McEachin <cmceachin5888@gmail.com>
2023-08-25 01:16:48 -0400

    added tests for billing and shipping address, updated respective db files and helpers.

--- db/models/billingAddress.js
+++ db/models/billingAddress.js
@@ -19,37 +19,113 @@ async function createBillingAddress(userId, street, city, state, postalCode, cou
 
 async function addBillingAddressToUser(userId, billingAddressList) {
   try {
-      const createdBillingAddresses = [];
-      for (const billingAddress of billingAddressList) {
-          const createdAddress = await createBillingAddress(
-              userId,
-              billingAddress.street,
-              billingAddress.city,
-              billingAddress.state,
-              billingAddress.postalCode,
-              billingAddress.country
-          );
-          createdBillingAddresses.push(createdAddress);
-      }
+    const createdBillingAddresses = [];
+    for (const billingAddress of billingAddressList) {
+      const createdAddress = await createBillingAddress(
+        userId,
+        billingAddress.street,
+        billingAddress.city,
+        billingAddress.state,
+        billingAddress.postalCode,
+        billingAddress.country
+      );
 
-      return createdBillingAddresses;
+      await client.query(
+        `
+        INSERT INTO user_billing_addresses (user_id, billing_address_id)
+        VALUES ($1, $2)
+        `,
+        [userId, createdAddress.id]
+      );
+
+      createdBillingAddresses.push(createdAddress);
+    }
+
+    return createdBillingAddresses;
   } catch (error) {
-      throw error;
+    throw new Error('Could not add billing address: ' + error.message);
   }
 }
 
 async function getBillingAddressByUserId(userId) {
   try {
-    const { rows } = await client.query(
-      `SELECT * 
-      FROM billing_addresses 
-      WHERE user_id = $1
-      `, 
-      [userId]);
-
+    const query = `
+      SELECT ba.id, ba.street, ba.city, ba.state, ba.postal_code, ba.country
+      FROM user_billing_addresses uba
+      JOIN billing_addresses ba ON uba.billing_address_id = ba.id
+      WHERE uba.user_id = $1
+    `;
+    
+    const { rows } = await client.query(query, [userId]);
     return rows;
   } catch (error) {
-    throw new Error('Could not get billing address: ' + error.message);
+    throw new Error('Could not get billing addresses: ' + error.message);
+  }
+}
+
+async function updateUserBillingAddress(userId, addressId, updatedAddressData) {
+  try {
+    const { street, city, state, postalCode, country } = updatedAddressData;
+
+    const query = `
+      UPDATE billing_addresses AS ba
+      SET street = $1, city = $2, state = $3, postal_code = $4, country = $5
+      FROM user_billing_addresses AS uba
+      WHERE uba.user_id = $6 AND uba.billing_address_id = $7 AND ba.id = uba.billing_address_id
+      RETURNING ba.*
+    `;
+
+    const values = [street, city, state, postalCode, country, userId, addressId];
+    const { rows: [updatedBillingAddress] } = await client.query(query, values);
+
+    if (!updatedBillingAddress) {
+      throw new Error(`Billing address with ID ${addressId} not found for user with ID ${userId}.`);
+    }
+
+    return updatedBillingAddress;
+  } catch (error) {
+    throw new Error('Could not update billing address: ' + error.message);
+  }
+}
+
+async function deleteBillingAddress(userId, addressId) {
+  try {
+    const billingAddress = await client.query(
+      `
+      SELECT user_id, billing_address_id
+      FROM user_billing_addresses
+      WHERE id = $1
+      `,
+      [addressId]
+    );
+
+    if (!billingAddress || billingAddress.rows.length === 0) {
+      throw new Error(`Billing address with ID ${addressId} not found.`);
+    }
+
+    if (billingAddress.rows[0].user_id !== userId) {
+      throw new Error('You do not have permission to delete this billing address.');
+    }
+
+    await client.query(
+      `
+      DELETE FROM user_billing_addresses
+      WHERE id = $1
+      `,
+      [addressId]
+    );
+
+    await client.query(
+      `
+      DELETE FROM billing_addresses
+      WHERE id = $1
+      `,
+      [billingAddress.rows[0].billing_address_id]
+    );
+
+    return true;
+  } catch (error) {
+    throw new Error('Could not delete billing address: ' + error.message);
   }
 }
 
@@ -57,5 +133,7 @@ async function getBillingAddressByUserId(userId) {
 module.exports = {
   createBillingAddress,
   getBillingAddressByUserId,
-  addBillingAddressToUser
+  addBillingAddressToUser,
+  updateUserBillingAddress,
+  deleteBillingAddress
 }
--- db/models/shippingAddress.js
+++ db/models/shippingAddress.js
@@ -19,43 +19,120 @@ async function createShippingAddress(userId, street, city, state, postalCode, co
 
 async function addShippingAddressToUser(userId, shippingAddressList) {
   try {
-      const createdShippingAddresses = [];
-      for (const shippingAddress of shippingAddressList) {
-          const createdAddress = await createShippingAddress(
-              userId,
-              shippingAddress.street,
-              shippingAddress.city,
-              shippingAddress.state,
-              shippingAddress.postalCode,
-              shippingAddress.country
-          );
-          createdShippingAddresses.push(createdAddress);
-      }
+    const createdShippingAddresses = [];
+    for (const shippingAddress of shippingAddressList) {
+      const createdAddress = await createShippingAddress(
+        userId,
+        shippingAddress.street,
+        shippingAddress.city,
+        shippingAddress.state,
+        shippingAddress.postalCode,
+        shippingAddress.country
+      );
 
-      return createdShippingAddresses;
+      await client.query(
+        `
+        INSERT INTO user_shipping_addresses (user_id, shipping_address_id)
+        VALUES ($1, $2)
+        `,
+        [userId, createdAddress.id]
+      );
+
+      createdShippingAddresses.push(createdAddress);
+    }
+
+    return createdShippingAddresses;
   } catch (error) {
-      throw error;
+    throw new Error('Could not add shipping address: ' + error.message);
   }
 }
 
 async function getShippingAddressByUserId(userId) {
   try {
-    const { rows } = await client.query(
-      `SELECT * 
-      FROM shipping_addresses 
-      WHERE user_id = $1
-      `, 
-      [userId]);
-
+    const query = `
+      SELECT ba.id, ba.street, ba.city, ba.state, ba.postal_code, ba.country
+      FROM user_shipping_addresses uba
+      JOIN shipping_addresses ba ON uba.shipping_address_id = ba.id
+      WHERE uba.user_id = $1
+    `;
+    
+    const { rows } = await client.query(query, [userId]);
     return rows;
   } catch (error) {
-    throw new Error('Could not get shipping address: ' + error.message);
+    throw new Error('Could not get shipping addresses: ' + error.message);
   }
 }
 
+async function updateUserShippingAddress(userId, addressId, updatedAddressData) {
+  try {
+    const { street, city, state, postalCode, country } = updatedAddressData;
+
+    const query = `
+      UPDATE shipping_addresses
+      SET street = $1, city = $2, state = $3, postal_code = $4, country = $5
+      FROM user_shipping_addresses uba
+      WHERE uba.user_id = $6 AND uba.shipping_address_id = $7
+      RETURNING *
+    `;
+
+    const values = [street, city, state, postalCode, country, userId, addressId];
+    const { rows: [updatedShippingAddress] } = await client.query(query, values);
+
+    if (!updatedShippingAddress) {
+      throw new Error(`Shipping address with ID ${addressId} not found for user with ID ${userId}.`);
+    }
+
+    return updatedShippingAddress;
+  } catch (error) {
+    throw new Error('Could not update shipping address: ' + error.message);
+  }
+}
+
+async function deleteShippingAddress(userId, addressId) {
+  try {
+    const shippingAddress = await client.query(
+      `
+      SELECT user_id, shipping_address_id
+      FROM user_shipping_addresses
+      WHERE id = $1
+      `,
+      [addressId]
+    );
+
+    if (!shippingAddress || shippingAddress.rows.length === 0) {
+      throw new Error(`Shipping address with ID ${addressId} not found.`);
+    }
+
+    if (shippingAddress.rows[0].user_id !== userId) {
+      throw new Error('You do not have permission to delete this shipping address.');
+    }
+
+    await client.query(
+      `
+      DELETE FROM user_shipping_addresses
+      WHERE id = $1
+      `,
+      [addressId]
+    );
+
+    await client.query(
+      `
+      DELETE FROM shipping_addresses
+      WHERE id = $1
+      `,
+      [shippingAddress.rows[0].shipping_address_id]
+    );
+
+    return true;
+  } catch (error) {
+    throw new Error('Could not delete shipping address: ' + error.message);
+  }
+}
 
 module.exports = {
   createShippingAddress,
   getShippingAddressByUserId,
-  addShippingAddressToUser
+  addShippingAddressToUser,
+  deleteShippingAddress,
+  updateUserShippingAddress
 }

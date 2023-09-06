3cc2b5d1ee9685dec6e92bbb26055c055b665bff
Vimaser <thfeske@gmail.com>
2023-09-06 03:06:26 -0500

    added search function

--- db/models/products.js
+++ db/models/products.js
@@ -1,64 +1,62 @@
-const client = require("./client");
-
-// Updated
+const client = require("../client");
 
 async function createProducts({
-    category,
-    brand,
-    name,
-    imageUrl,
-    description,
-    min_price,
-    max_price,
-    currency_code,
-    amount,
-    availability,
-    total_inventory,
+  categoryId,
+  brand,
+  name,
+  imageUrl,
+  description,
+  min_price,
+  max_price,
+  currency_code,
+  amount,
+  availability,
+  total_inventory,
 }) {
-  try {
-    const {
-      rows: [products],
-    } = await client.query(
-      ` INSERT INTO products(category,
-        brand,
-        name,
-        imageUrl,
-        description,
-        min_price,
-        max_price,
-        currency_code,
-        amount,
-        availability,
-        total_inventory, )
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
-            RETURNING *;
-            `,
-      [category,
-        brand,
-        name,
-        imageUrl,
-        description,
-        min_price,
-        max_price,
-        currency_code,
-        amount,
-        availability,
-        total_inventory]
-    );
-    return products;
-  } catch (error) {
-    throw error;
-  }
+try {
+  const {
+    rows: [product],
+  } = await client.query(
+    ` INSERT INTO products(category_id,
+      brand,
+      name,
+      imageUrl,
+      description,
+      min_price,
+      max_price,
+      currency_code,
+      amount,
+      availability,
+      total_inventory )
+          VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
+          RETURNING *;
+          `,
+    [categoryId,
+      brand,
+      name,
+      imageUrl,
+      description,
+      min_price,
+      max_price,
+      currency_code,
+      amount,
+      availability,
+      total_inventory]
+  );
+  return product;
+} catch (error) {
+  throw new Error('Could not create product: ' + error.message);
+}
 }
 
 async function getProductById(id) {
   try {
     const { rows } = await client.query(
       `
-            SELECT *
-            FROM products
-            WHERE id = $1;
-            `,
+      SELECT *
+      FROM products
+      WHERE id = $1;
+      `,
       [id]
     );
 
@@ -68,108 +66,135 @@ async function getProductById(id) {
 
     return rows[0];
   } catch (error) {
-    throw error;
+    throw new Error('Could not locate product: ' + error.message);
   }
 }
 
 async function getProductsWithoutOrders() {
   try {
     const { rows } = await client.query(`
-            SELECT *
-            FROM products
-            LEFT JOIN product_orders ON product.id = product_orders."product.id"
-            WHERE product_orders.id IS NULL;
-        `);
+      SELECT *
+      FROM products
+      LEFT JOIN product_orders ON product.id = product_orders."product.id"
+      WHERE product_orders.id IS NULL;
+    `);
+
     if (rows.length === 0) {
       return null;
     }
 
     return rows;
   } catch (error) {
-    throw error;
+    throw new Error('Could not locate products: ' + error.message);
   }
 }
 
 async function getAllProducts() {
-  // temporary until .db/orders.js is finished
   try {
     const { rows: products } = await client.query(`
-            SELECT *
-            FROM products;
-        `);
-  } catch (error) {}
+      SELECT *
+      FROM products;
+    `);
+
+    return products;
+  } catch (error) {
+    throw new Error('Could not locate products: ' + error.message);
+  }
 }
 
-async function getAllProductsByOrders({ orders }) {
-  // Needs to built out after orders
+async function updateProduct({productId, updatedFields}) {
   try {
-  } catch (error) {}
-}
+    const {  
+      brand, 
+      name, 
+      imageUrl, 
+      description , 
+      min_price, 
+      max_price, 
+      currency_code, 
+      amount, 
+      availability,
+      total_inventory     
+    } = updatedFields;
 
-async function getProductsByOrdered({ id }) {
-  // fleshout after db/orders.js
-  try {
-  } catch (error) {}
-}
+    const existingProduct = await getProductById(productId);
+    if (!existingProduct) {
+      throw new Error(`Product with ID ${productId} not found.`);
+    }
 
-async function updateProduct({ id, ...fields }) {
-  try {
-    const updateFields = Object.keys(fields)
+    const updateFields = Object.keys(updatedFields)
       .map((key, index) => `"${key}" = $${index + 1}`)
       .join(", ");
 
-    const values = Object.values(fields);
+    const values = Object.values(updatedFields);
 
     const { rows } = await client.query(
       `
-                UPDATE products
-                SET ${updateFields}
-                WHERE id=$${values.length + 1}
-                RETURNING *;
-                `,
-      [...values, id]
+      UPDATE products
+      SET ${updateFields}
+      WHERE id=$${values.length + 1}
+      RETURNING *;
+      `,
+      [...values, productId]
     );
-    const updateProduct = rows[0];
+    const updatedProduct = rows[0];
 
-    return updateProduct;
+    return updatedProduct;
   } catch (error) {
-    throw error;
+    throw new Error('Could not update product: ' + error.message);
   }
 }
 
 async function destroyProduct(id) {
-    try {
-        const deletedProduct = await getProductById(id);
+  try {
+    const deletedProduct = await getProductById(id);
 
-        if (!deletedProduct) {
-            return null;
-        }
-
-        await client.query(
-            `
-            DELETE FROM product_orders
-            WHERE "productId" = $1
-            `,
-            [id]
-        );
-
-        const { rowCount } = await client.query(
-            `
-            DELETE FROM products
-            WHERE id = $1
-            `,
-            [id]
-        );
-
-        if (rowCount === 0) {
-            return null;
-        }
-
-        return deletedProduct;
-    } catch (error) {
-        throw error;
+    if (!deletedProduct) {
+      return null;
     }
+
+    await client.query(
+      `
+      DELETE FROM order_history
+      WHERE $1 = ANY(order_products)
+      `,
+      [id]
+    );
+
+    const { rowCount } = await client.query(
+      `
+      DELETE FROM products
+      WHERE id = $1
+      `,
+      [id]
+    );
+
+    if (rowCount === 0) {
+      return null;
+    }
+
+    return deletedProduct;
+  } catch (error) {
+    throw new Error('Could not delete product: ' + error.message);
+  }
 }
+
+async function searchProducts(query) {
+  try {
+      const { rows: products } = await client.query(`
+          SELECT *
+          FROM products
+          WHERE name ILIKE $1
+          OR description ILIKE $1;
+      `, [`%${query}%`]);
+
+      return products;
+  } catch (error) {
+      throw new Error('Could not search products: ' + error.message);
+  }
+}
+
+
 
 module.exports = {
   createProducts,
@@ -177,5 +202,6 @@ module.exports = {
   getProductsWithoutOrders,
   getAllProducts,
   updateProduct,
-  destroyProduct
+  destroyProduct,
+  searchProducts
 };
